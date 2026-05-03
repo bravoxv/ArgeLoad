6dd70a0057a163f8a7df486feeef7e2668b807c8
bravoxv <lautaro.dbbrp@gmail.com>
2026-05-03 18:17:15 -0300

    imagen

--- src/App.tsx
+++ src/App.tsx
@@ -144,7 +144,7 @@ export default function App() {
       <main className="max-w-md mx-auto py-8">
 
         <header className="flex flex-col items-center text-center space-y-4 mb-8">
-          <img src="/logo_bxv.png" alt="BXV Logo" className="w-28 h-28 object-contain mix-blend-screen pointer-events-none drop-shadow-2xl" />
+          <img src="/logo_bxv_v2.png" alt="BXV Logo" className="w-28 h-28 object-contain mix-blend-screen pointer-events-none drop-shadow-2xl" />
           <div>
             <h1 onClick={() => setClicks(c => c + 1)} className="text-4xl font-black tracking-tighter text-white select-none relative cursor-pointer">
               ArgeLoad
